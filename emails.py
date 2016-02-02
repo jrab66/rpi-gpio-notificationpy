7364442bfb0222c0257acacea5e8ede354fd2127
Jose Barahona <jrab6692@gmail.com>
2016-02-02 10:56:32 -0600

    readme

--- emails.py
+++ emails.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*--
+
+'''
+Written by Jose Barahona
+www.github.com/jrab66
+February 02, 2016
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+'''
+
+
 import smtplib
 
 #mime import email modules
--- state.py
+++ state.py
@@ -1,8 +1,22 @@
+'''
+Written by Jose Barahona
+www.github.com/jrab66
+February 02, 2016
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+'''
+
 import RPi.GPIO as GPIO
 import time
 import os
 
-#adjust for where your switch is connected
+#adjust for where your switch is connected Can be any GPIO.
 buttonPin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
